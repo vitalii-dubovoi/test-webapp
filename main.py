74c2e0d8dc8c22a29d190542ca294d2559723bb2
Vitalii Dubovoy <vitalii@reply.io>
2024-10-11 15:27:06 +0200

    Fri Oct 11 15:27:06 CEST 2024

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
 @app.route('/app', methods=['GET'])
-def get_client_ip():
-    return f"Hello there"
+def get_client_headers():
+    # Get all request headers from the client
+    headers = {key: value for key, value in request.headers.items()}
+    return jsonify(headers)
 
 @app.route('/health', methods=['GET'])
 def health_check():
