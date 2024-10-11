32bbfbb198c8a1f8357f96966740c2d2d6c2ceb3
Vitalii Dubovoy <vitalii@reply.io>
2024-10-11 13:18:07 +0200

    Fri Oct 11 13:18:07 CEST 2024

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ app = Flask(__name__)
 
 @app.route('/app', methods=['GET'])
 def get_client_ip():
-    # Get the client's IP address
-    client_ip = request.remote_addr
-    # Return the response with the client's IP
+    # Check if the request has the 'X-Forwarded-For' header
+    if 'X-Forwarded-For' in request.headers:
+        # Get the first IP in the 'X-Forwarded-For' header, which is the client's real IP
+        client_ip = request.headers['X-Forwarded-For'].split(',')[0].strip()
+    else:
+        # Fall back to the direct remote address if 'X-Forwarded-For' is not present
+        client_ip = request.remote_addr
+    
     return f"Hello there, your IP is - {client_ip}"
 
 @app.route('/health', methods=['GET'])
 def health_check():
-    # Simply return a 200 OK status
     return 'OK', 200
 
 if __name__ == '__main__':
